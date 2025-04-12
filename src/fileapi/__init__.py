8bc828e9bfddc6f79601f5fffb7db9f2ba2581df
Parth Patel <parth.p@mendel.ai>
2025-04-12 01:09:18 -0700

    feat: added open command

--- src/fileapi/__init__.py
+++ src/fileapi/__init__.py
@@ -1,3 +1,12 @@
 from .__version__ import __version__
 from .fileapi import *
 from .storage_options import *
+
+
+@contextmanager
+def open(path: Union[str, FileAPI], mode: str):
+    """
+    Open a file with the given path and mode.
+    """
+    with FileAPI.apply(path=path).create_input_stream(mode=mode) as io_stream:
+        yield io_stream
